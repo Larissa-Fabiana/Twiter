08bb90c402d33da82878dd076e71d570fe819644
Larissa Fabiana F S <ferreira-larissa@outlook.com>
2018-07-09 00:19:46 -0300

    versao 0.0.2 item 2

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-
-function transformInTweet(event){
-    var tweet = document.getElementsByClassName("tweets")[0];
-    // console.log(tweet); sua mensagem aqui
+var tweet = document.getElementsByClassName("tweets")[0];
+var myButton = document.getElementsByClassName("btn")[0];
+var mensage = document.getElementsByClassName("mensages")[0];
+var paragraph = document.getElementById("paragraphs");
+var counter = document.getElementById("count");
+tweet.addEventListener("keyup", event =>{
+    event.preventDefault();
+    numberCounter();
+});
+function numberCounter(){
+    var sum = 140;
+    var text = tweet.value;
+    for(var i = 0; i<text.length; i++){ 
+        sum = sum - 1;      
+    }
+    counter.innerHTML = sum;
+}
+textEntered();
+function textEntered(){
+    myButton.setAttribute("id", "pressed");
+    pressed.addEventListener("click", event => {
+        event.preventDefault();
+        transformInTweet();
+    });
+}
+function transformInTweet(){
     var div = document.createElement("div");
     div.className = "post";
     div.innerHTML = tweet.value;
-    // console.log(div.textContent);
-    //div vai ser filha do id mensages
-    var mensage = document.getElementById("mensages");
-    // console.log(mensage);
     mensage.appendChild(div);
-    // console.log(event);
-    event.preventDefault();
-    var myButton = document.getElementById("btn");
-    console.log(myButton.innerHTML);
-
 }
